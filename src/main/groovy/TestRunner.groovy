ca320eabd6053e0e6be11f4f3a9666ab864eb00a
beomWhale <cndqjacndqja@gmail.com>
2022-07-30 07:12:29 +0900

    refactor: gradle 의존성 nGrinder 최신 버전으로 변경

--- src/main/groovy/TestRunner.groovy
+++ src/main/groovy/TestRunner.groovy
@@ -1,25 +1,20 @@
-import HTTPClient.NVPair
-
-import static net.grinder.script.Grinder.grinder
-import static org.junit.Assert.*
-import static org.hamcrest.Matchers.*
-import net.grinder.script.GTest
-import net.grinder.script.Grinder
 import groovy.json.JsonSlurper
+import net.grinder.plugin.http.HTTPRequest
+import net.grinder.script.GTest
 import net.grinder.scriptengine.groovy.junit.GrinderRunner
 import net.grinder.scriptengine.groovy.junit.annotation.BeforeProcess
 import net.grinder.scriptengine.groovy.junit.annotation.BeforeThread
-// import static net.grinder.util.GrinderUtils.* // You can use this if you're using nGrinder after 3.2.3
 import org.junit.Before
-import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.ngrinder.http.HTTPRequest
 import org.ngrinder.http.HTTPRequestControl
 import org.ngrinder.http.HTTPResponse
 import org.ngrinder.http.cookie.Cookie
 import org.ngrinder.http.cookie.CookieManager
+
+import static net.grinder.script.Grinder.grinder
+import static org.hamcrest.Matchers.is
+import static org.junit.Assert.assertThat
 
 /**
  * A simple example using the HTTP plugin that shows the retrieval of a single page via HTTP.
